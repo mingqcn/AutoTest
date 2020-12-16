dad40744c7dc93b392c9fa0aca4d2ce04a7a2e40
mingqcn <jiraxmu@163.com>
2020-12-16 12:54:05 +0800

    v-on

--- src/main/java/cn/edu/xmu/testserver/controller/TestController.java
+++ src/main/java/cn/edu/xmu/testserver/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.edu.xmu.testserver.controller;
 
 import cn.edu.xmu.testserver.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +19,18 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    private  static  final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
     @PostMapping("/ajax/newtask")
     public Object newtask(@Validated @RequestBody TaskVo taskVo) throws Throwable {
+        logger.debug("newtask: taskVo = "+taskVo);
         String path =  testService.newtask(taskVo.getGroupname(), taskVo.getAdminGateway(), taskVo.getMallGateway());
         Map<String, String> ret = new HashMap<>(1);
         ret.put("path",path);
+        logger.debug("newtask: ret = "+ret);
         return ret;
     }
 }
