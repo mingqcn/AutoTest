4a1fdc062e2caa1ae9f9879972034913a51729df
mingqcn <jiraxmu@163.com>
2020-12-16 14:05:53 +0800

    v-on

--- src/main/java/cn/edu/xmu/testserver/controller/PageController.java
+++ src/main/java/cn/edu/xmu/testserver/controller/PageController.java
@@ -3,10 +3,14 @@ package cn.edu.xmu.testserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
 @Controller
 public class PageController {
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("index.html");
     }
 }
