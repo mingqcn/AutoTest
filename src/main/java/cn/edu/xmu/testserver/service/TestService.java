ec5a03065e04c138eabb42144cda0844d5772cea
mingqcn <jiraxmu@163.com>
2020-12-16 13:24:41 +0800

    v-on

--- src/main/java/cn/edu/xmu/testserver/service/TestService.java
+++ src/main/java/cn/edu/xmu/testserver/service/TestService.java
@@ -27,6 +27,6 @@ public class TestService {
         }catch (IOException e){
             logger.error("newtask: msg = "+e.getMessage());
         }
-        return groupName+"/"+dir;
+        return "http://172.16.1.66/st/"+groupName+"/"+dir;
     }
 }
