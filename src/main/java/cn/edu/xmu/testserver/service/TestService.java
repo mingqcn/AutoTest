30dedb9976109e521b0fd6101e72f0854e1cca22
Mingqiu <mingqiu@xmu.edu.cn>
2020-12-16 23:57:17 +0800

    SSH + EXEC

--- src/main/java/cn/edu/xmu/testserver/service/TestService.java
+++ src/main/java/cn/edu/xmu/testserver/service/TestService.java
@@ -28,24 +28,32 @@ public class TestService {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.systemDefault());
         String dir = LocalDateTime.now().format(format);
         logger.debug("newtask: groupName = "+groupName+",dir = "+ dir +", manageGate="+manageGate+", mallGate="+mallGate);
-//        String [] cmd = {"sh","/home/mybaby/privilege/public-test/runtest.sh",groupName, dir, manageGate, mallGate};
-//        try {
-//            Process proc = Runtime.getRuntime().exec(cmd);
-    //        //取得命令结果的输出流
-    //        InputStream fis=proc.getInputStream();
-    //        //用一个读输出流类去读
-    //        InputStreamReader isr=new InputStreamReader(fis);
-    //        //用缓冲器读行
-    //        BufferedReader br=new BufferedReader(isr);
-    //        String line=null;
-    //        //直到读完为止
-    //        while((line=br.readLine())!=null)
-    //        {
-    //            logger.debug(line);
-    //        }
-//        }catch (IOException e){
-//            logger.error("newtask: msg = "+e.getMessage());
-//        }
+        String [] cmd = {"sh","/home/mybaby/privilege/public-test/runtest.sh",groupName, dir, manageGate, mallGate};
+        try {
+            Process proc = Runtime.getRuntime().exec(cmd);
+            //取得命令结果的输出流
+            InputStream fis=proc.getInputStream();
+            //用一个读输出流类去读
+            InputStreamReader isr=new InputStreamReader(fis);
+            //用缓冲器读行
+            BufferedReader br=new BufferedReader(isr);
+            String line=null;
+            //直到读完为止
+            while((line=br.readLine())!=null)
+            {
+                logger.debug(line);
+            }
+        }catch (IOException e){
+            logger.error("newtask: msg = "+e.getMessage());
+        }
+        ssh.runNew(groupName, dir, manageGate, mallGate);
+        return "http://172.16.1.66/test/"+groupName+"/"+dir;
+    }
+
+    public String newSSHtask(String groupName, String manageGate, String mallGate){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.systemDefault());
+        String dir = LocalDateTime.now().format(format);
+        logger.debug("newtask: groupName = "+groupName+",dir = "+ dir +", manageGate="+manageGate+", mallGate="+mallGate);
         ssh.runNew(groupName, dir, manageGate, mallGate);
         return "http://172.16.1.66/test/"+groupName+"/"+dir;
     }
