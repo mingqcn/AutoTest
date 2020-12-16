7541686ef7a3f163d22f165270bf18a7194e450d
mingqcn <jiraxmu@163.com>
2020-12-16 13:39:04 +0800

    v-on

--- src/main/java/cn/edu/xmu/testserver/controller/TestController.java
+++ src/main/java/cn/edu/xmu/testserver/controller/TestController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,10 @@ public class TestController {
         ret.put("path",path);
         logger.debug("newtask: ret = "+ret);
         return ret;
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
