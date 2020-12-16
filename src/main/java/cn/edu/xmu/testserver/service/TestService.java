c66ac8ea14809d6577ace74ee01474d1833db55c
mingqcn <jiraxmu@163.com>
2020-12-16 14:34:39 +0800

    v-on

--- src/main/java/cn/edu/xmu/testserver/service/TestService.java
+++ src/main/java/cn/edu/xmu/testserver/service/TestService.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -24,6 +27,18 @@ public class TestService {
         logger.debug("newtask: cmd = "+cmd.toString());
         try {
             Process proc = Runtime.getRuntime().exec(cmd);
+            //取得命令结果的输出流
+            InputStream fis=proc.getInputStream();
+            //用一个读输出流类去读
+            InputStreamReader isr=new InputStreamReader(fis);
+            //用缓冲器读行
+            BufferedReader br=new BufferedReader(isr);
+            String line=null;
+            //直到读完为止
+            while((line=br.readLine())!=null)
+            {
+              logger.debug(line);
+            }
         }catch (IOException e){
             logger.error("newtask: msg = "+e.getMessage());
         }
